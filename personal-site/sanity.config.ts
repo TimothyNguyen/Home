e919613dfc16bf895b8b4b878f24434c4aaab2ff
Timothy Nguyen <quynhthoa1972@gmail.com>
2023-11-25 09:14:50 -0500

    Delete old code and update env variables

--- personal-site/sanity.config.ts
+++ personal-site/sanity.config.ts
@@ -1,12 +1,13 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
+import { dataset, projectId } from './sanity/env'
 
 export default defineConfig({
   name: 'default',
   title: "Timothy Nguyen",
-  projectId: 't6caphan',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
   basePath: "/studio",
   plugins: [deskTool()],
   schema: {
--- sanity-studio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 't6caphan',
-    dataset: 'production'
-  }
-})
