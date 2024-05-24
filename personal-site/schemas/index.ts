42f44c93f89fa8a57d41e87fa000e3aa7e07773b
VisualAlgo <visualalgocode@gmail.com>
2024-05-23 23:41:32 -0700

    Fix build

--- personal-site/schemas/index.ts
+++ personal-site/schemas/index.ts
@@ -4,8 +4,5 @@ import gridItem from "./gridItem";
 import gridItemLayout from "./gridItemLayout";
 import GridItemType from "./gridItemType";
 import profile from "./singletons/profile";
-import timeline from "./timeline";
-import milestone from "./milestone";
-import duration from "./duration";
 
-export const schemaTypes = [profile, gridItem, gridItemLayout, GridItemType, milestone, timeline, duration];
+export const schemaTypes = [profile, gridItem, gridItemLayout, GridItemType];
